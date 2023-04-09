c728fee349742e4133a621434ca841e8518ed220
Ekaterina Eremina <31165386+Yaroina@users.noreply.github.com>
2023-04-09 10:16:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,14 +5,7 @@ import numpy as np
 chat_id = 395384260 # Ваш chat ID, не меняйте название переменной
 
     def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     alpha = 0.07
-    
-    pval = stats.ttest_ind(x, y).pvalue
-    if pval < alpha:
-      res = True
-    else:
-      res = False    
-    return res 
+    from scipy.stats import mannwhitneyu
+    p = mannwhitneyu(x, y, alternative='greater')[1]
+    return p < alpha
